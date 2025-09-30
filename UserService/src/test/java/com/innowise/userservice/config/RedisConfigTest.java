b9a4f445748baeee3d07a33ac5e0d98e27c6f852
dshparko <951008d@mail.ru>
2025-09-30 13:42:58 +0300

    USR-6 : add tests for config

---
+++ UserService/src/test/java/com/innowise/userservice/config/RedisConfigTest.java
@@ -0,0 +1,49 @@
+package com.innowise.userservice.config;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.innowise.userservice.dto.UserDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+class RedisConfigTest {
+
+    @Autowired
+    private ObjectMapper redisObjectMapper;
+
+    @Test
+    void shouldSerializeAndDeserializeUserDtoCorrectly() throws Exception {
+        UserDto dto = new UserDto(1L, "Darya", "Shparko", "darya@example.com", LocalDate.of(1995, 1, 1), List.of());
+
+        String json = redisObjectMapper.writeValueAsString(dto);
+        UserDto deserialized = redisObjectMapper.readValue(json, UserDto.class);
+
+        assertThat(deserialized.id()).isEqualTo(1L);
+        assertThat(deserialized.name()).isEqualTo("Darya");
+        assertThat(deserialized.email()).isEqualTo("darya@example.com");
+    }
+
+    @Test
+    void shouldIgnoreUnknownProperties() throws Exception {
+        String json = """
+            {
+              "id": 2,
+              "name": "Ivan",
+              "surname": "Ivanov",
+              "email": "ivan@example.com",
+              "birthDate": "1990-05-15",
+              "unknownField": "ignored"
+            }
+            """;
+
+        UserDto dto = redisObjectMapper.readValue(json, UserDto.class);
+        assertThat(dto.name()).isEqualTo("Ivan");
+    }
+}
