2bf3eab949779a973311690fefa1a7aaf85e66e9
dshparko <951008d@mail.ru>
2025-10-08 17:53:49 +0300

    fix: try to fix sonar error

--- UserService/src/main/java/com/innowise/userservice/config/SecurityConfig.java
+++ UserService/src/main/java/com/innowise/userservice/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -28,8 +29,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/api/**"))
+                .csrf(AbstractHttpConfigurer::disable)// NOSONAR: Stateless JWT-based API, CSRF not applicable
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated()
                 )
